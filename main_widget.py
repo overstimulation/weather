885ccccd1f8a27ed46c613a66a0486abb96402ce
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-03-28 11:05:07 +0100

    Lab5: Zad3

--- main_widget.py
+++ main_widget.py
@@ -1,6 +1,15 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLineEdit, QPushButton, QLabel, QGridLayout
+
 
 class MainWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle('Weather')
+        city_edit = QLineEdit(self)
+        get_cities_button = QPushButton("Get cities", self)
+        weather_label = QLabel(self)
+
+        layout = QGridLayout(self)
+        layout.addWidget(city_edit, 0, 0)
+        layout.addWidget(get_cities_button, 0, 1)
+        layout.addWidget(weather_label, 1, 0, 1, 2)
