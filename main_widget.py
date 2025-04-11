761b03434fb3a45c99affefe33499749d335477a
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-11 12:12:24 +0200

    Lab6: Zad6

--- main_widget.py
+++ main_widget.py
@@ -1,8 +1,9 @@
-from PySide6.QtCore import QSettings
+from PySide6.QtCore import QSettings, QTimer
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QWidget, QLineEdit, QPushButton, QLabel, QGridLayout, QMessageBox, QListWidget, \
     QListWidgetItem
 import requests
+
 
 from city_list_item import CityListItem
 from settings_dialog import SettingsDialog
@@ -18,11 +19,14 @@ class MainWidget(QWidget):
         self.city_list = QListWidget(self)
         settings_button = QPushButton("Settings", self)
         self.favourite_city_list = QListWidget(self)
+        self.timer = QTimer()
 
         get_cities_button.clicked.connect(self.get_cities)
         settings_button.clicked.connect(self.show_settings)
         #self.city_list.itemClicked.connect(self.get_weather)
         self.city_list.itemDoubleClicked.connect(self.add_item_to_favourites)
+        self.timer.timeout.connect(self.get_weather)
+        self.timer.start(30000)
 
 
         layout = QGridLayout(self)
@@ -34,6 +38,7 @@ class MainWidget(QWidget):
         layout.addWidget(self.favourite_city_list, 1, 1, 1, 1)
 
         self.load_persistent_cities()
+        self.get_weather()
 
         # self.weather_params = {"temperature_2m": True}
 
@@ -55,8 +60,17 @@ class MainWidget(QWidget):
 
     def get_weather(self):
         # print(self.city_list.currentItem().data(Qt.UserRole))
-        latitude = self.city_list.currentItem().latitude
-        longitude = self.city_list.currentItem().longitude
+        latitude = []
+        longitude =[]
+        if self.favourite_city_list.count() == 0:
+            return
+        for row in range(self.favourite_city_list.count()):
+            item = self.favourite_city_list.item(row)
+            latitude.append(str(item.latitude))
+            longitude.append(str(item.longitude))
+        latitude = ','.join(latitude)
+        longitude = ','.join(longitude)
+
         keys = ''
         settings = QSettings()
         settings.beginGroup('parameters')
@@ -64,9 +78,15 @@ class MainWidget(QWidget):
             if settings.value(key, type=bool) == True:
                 keys += key + ','
         url=f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current={keys}"
+        #print(url)
         response = requests.get(url)
         json = response.json()
-        self.weather_label.setText(str(json['current']))
+        weather_text = []
+        for city in json:
+            print(city)
+            weather_text.append(str(city["current"]))
+        self.weather_label.setText('\n'.join(weather_text))
+        #print(json)
 
     def show_settings(self):
         settings_dialog = SettingsDialog()
@@ -111,11 +131,13 @@ class MainWidget(QWidget):
             data = item.dump()
             result.append(data.decode('utf-8'))
 
-        settings.setValue('parameters/cities', ';'.join(result))
+        settings.setValue('cities/cities', ';'.join(result))
 
     def load_persistent_cities(self):
         settings = QSettings()
-        coded_cities = settings.value('parameters/cities', '', type=str)
+        coded_cities = settings.value('cities/cities', type=str)
+        if len(coded_cities) == 0:
+            return
         for coded_city in coded_cities.split(';'):
             city = CityListItem.from_dump(coded_city)
             self.favourite_city_list.addItem(city)
